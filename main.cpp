0283ea76dd6fbbd9627b3666384c5cc29c9f1e9a
mjkim <kokome2@naver.com>
2023-09-28 20:03:20 +0900

    kokome2@naver.com

--- main.cpp
+++ main.cpp
@@ -1,44 +1,77 @@
-//
-// Created by 김명재 on 2023/05/29.
-//
-#include <cppcms/application.h>
-#include <cppcms/applications_pool.h>
-#include <cppcms/service.h>
-#include <cppcms/http_response.h>
 #include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
 
-#include <boost/log/trivial.hpp>
-
-
-class hello_world_test : public cppcms::application {
+class Content {
 public:
-    hello_world_test(cppcms::service &srv) : cppcms::application(srv) { }
-    virtual void main(std::string url);
+    std::string title;
+    std::vector<std::string> tags;
+
+    Content(std::string t, std::vector<std::string> tg) : title(t), tags(tg) {}
 };
 
-void hello_world_test ::main(std::string /*url*/)
-{
-    response().out() << "hello_world_test";
-}
+class User {
+public:
+    std::vector<std::string> liked_tags;
 
-int main(int argc, char* argv[]) {
-
-//    boost log sample
-//    BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-//    BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-//    BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-//    BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-//    BOOST_LOG_TRIVIAL(error) << "An error severity message";
-//    BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
-
-    try {
-        cppcms::service srv(argc, argv);
-        srv.applications_pool().mount(
-                cppcms::create_pool<hello_world_test>()
-        );
-        srv.run();
+    void likeTag(std::string tag) {
+        liked_tags.push_back(tag);
     }
-    catch(std::exception const &e) {
-        std::cerr << e.what() << std::endl;
+};
+
+class RecommenderSystem {
+    std::vector<Content> contents;
+
+public:
+    RecommenderSystem(std::vector<Content> c) : contents(c) {}
+
+    std::vector<Content> recommend(User user) {
+        std::map<std::string, int> scores;
+
+        // 각 컨텐츠에 대한 사용자의 선호 점수를 계산
+        for (auto &content : contents) {
+            for (auto &tag : content.tags) {
+                if (std::find(user.liked_tags.begin(), user.liked_tags.end(), tag) != user.liked_tags.end()) {
+                    scores[content.title]++;
+                }
+            }
+        }
+
+        // 점수에 따라 컨텐츠를 정렬
+        std::vector<Content> recommendations;
+        for (auto &content : contents) {
+            if (scores[content.title] > 0) {
+                recommendations.push_back(content);
+            }
+        }
+
+        std::sort(recommendations.begin(), recommendations.end(), [&](const Content &a, const Content &b) {
+            return scores[a.title] > scores[b.title];
+        });
+
+        return recommendations;
     }
+};
+
+int main() {
+    std::vector<Content> contents = {
+        Content("Movie A", {"action", "comedy"}),
+        Content("Movie B", {"drama", "romance"}),
+        Content("Movie C", {"action", "thriller"})
+    };
+
+    User user;
+    user.likeTag("action");
+
+    RecommenderSystem system(contents);
+    auto recommendations = system.recommend(user);
+
+    std::cout << "Recommendations for user:" << std::endl;
+    for (auto &content : recommendations) {
+        std::cout << content.title << std::endl;
+    }
+
+    return 0;
 }
--- src/client/cppcms_sample_client.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 김명재 on 2023/06/04.
-//
-
-#include "../../include/cppcms_sample_client.h"
